defbe3c684286897a12c31f1f447b8fc9e9dd6f4
Stacey Burns <k00203620@student.lit.ie>
2016-09-27 15:33:43 +0100

    form update

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
 
 <body>
 <?php
+	$name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 	$from = 'From: Portfolio website'; 
     $to = 'stceburns@gmail.com'; 
     $subject = 'PF SITE Message';
 	
-	$body = "From: E-Mail: $email\n Message:\n $message";
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 	
 	if ($_POST['submit']) {
     if (mail ($to, $subject, $body, $from)) { 
