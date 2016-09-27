76c337c13891fce2ba83c6d65a18335bf2408ed6
Stacey Burns <k00203620@student.lit.ie>
2016-09-27 15:40:42 +0100

    2

--- index.php
+++ index.php
@@ -7,22 +7,23 @@
 
 <body>
 <?php
-	$name = $_POST['name'];
+    $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-	$from = 'From: Portfolio website'; 
+    $from = 'From: PF site'; 
     $to = 'stceburns@gmail.com'; 
-    $subject = 'PF SITE Message';
-	
-	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
-	
-	if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+    $subject = 'PF SITE message';
+
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+    if ($_POST['submit']) {				 
+        if (mail ($to, $subject, $body, $from)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
     }
-}
 ?>
 </body>
 </html>
