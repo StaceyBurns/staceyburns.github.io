a9a0dfeed9732b0a00894507e0c16cf83c4eace0
Stacey Burns <k00203620@student.lit.ie>
2016-10-26 17:15:56 +0100

    Populating
    Adding content and pages for work. Bug fixes

---
+++ js/staceyburns.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+/*function addDiamond() {
+    if($(window).innerWidth() > 960) {
+        $(".diamondswrap > div:nth-child(7)").after("<a> </a>");
+		 $(".diamondswrap > div:nth-child(8)").addClass('item');
+		 $(".diamondswrap > div:nth-child(8)").append('<img id="emptyD" <img src="images/ph-kegs.jpg"/>');
+		
+    } else {
+        $('body').addClass('small-screen').removeClass('big-screen');
+    }
+}
+
+addDiamond();
+
+$(window).bind('resize',function(){
+    addDiamond();
+});*/
